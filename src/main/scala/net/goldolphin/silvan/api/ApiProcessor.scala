b32268884604e72150f207d9d3f0dc546c65de2c
caofx <caofx@rd.netease.com>
2012-07-18 19:59:52 +0800

    ADD: src

---
+++ src/main/scala/net/goldolphin/silvan/api/ApiProcessor.scala
@@ -0,0 +1,15 @@
+package net.goldolphin.silvan.api
+
+import net.goldolphin.silvan.avro.{Message, SilvanApi}
+
+/**
+ * Author: caofx
+ * Date: 2012-07-18
+ */
+
+object ApiProcessor extends SilvanApi {
+
+  def send(message: Message): CharSequence = {
+    null
+  }
+}
